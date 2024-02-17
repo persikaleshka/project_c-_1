14ffdfe506cb5c456bc4ab96f781b3cdb398c1c7
Alenka Sharipova <alenkasharipova@MacBook-Air-Alenka.local>
2024-02-17 18:33:13 +0300

    add class BigNumber and operators

---
+++ BigNumber.h
@@ -0,0 +1,33 @@
+#ifndef BIGNUMBER_H
+#define BIGNUMBER_H
+
+#include <string>
+#include <iostream>
+
+class BigNumber {
+public:
+    BigNumber();                            
+    BigNumber(const std::string& num);      
+    BigNumber(double value);                
+
+    // Операторы арифметики
+    BigNumber operator+(const BigNumber& other) const; 
+    BigNumber operator-(const BigNumber& other) const; 
+    BigNumber operator*(const BigNumber& other) const; 
+    BigNumber operator/(const BigNumber& other) const; 
+
+    // Операторы сравнения
+    bool operator==(const BigNumber& other) const; 
+    bool operator!=(const BigNumber& other) const; 
+    bool operator<(const BigNumber& other) const;  
+    bool operator>(const BigNumber& other) const;  
+    bool operator<=(const BigNumber& other) const; 
+    bool operator>=(const BigNumber& other) const; 
+
+    std::string toString() const;
+
+private:
+    std::string number; 
+};
+
+#endif 
