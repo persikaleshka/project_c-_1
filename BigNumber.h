abf73a44bf7341d9620fb490f3923f0af6168d4c
Alenka Sharipova <alenkasharipova@MacBook-Air-Alenka.local>
2024-02-20 00:53:56 +0300

    new operators -number +number

--- BigNumber.h
+++ BigNumber.h
@@ -6,30 +6,36 @@
 
 class BigNumber {
 public:
+    std::string integer; 
+    std::string fractional; 
+    bool sign; 
+
     BigNumber();                            
     BigNumber(const std::string& num);      
     BigNumber(double value);                
 
     // Операторы арифметики
-    BigNumber operator+(const BigNumber& other) const; 
-    BigNumber operator-(const BigNumber& other) const; 
-    BigNumber operator*(const BigNumber& other) const; 
-    BigNumber operator/(const BigNumber& other) const; 
+    BigNumber operator - () const;
+    BigNumber operator + () const;
+    friend BigNumber operator + (const BigNumber& first, const BigNumber& second); 
+    friend BigNumber operator - (const BigNumber& first,  const BigNumber& second); 
+    friend BigNumber operator * (const BigNumber& first, const BigNumber& second); 
+    friend BigNumber operator / (const BigNumber& first, const BigNumber& second); 
 
     // Операторы сравнения
-    bool operator==(const BigNumber& other) const; 
-    bool operator!=(const BigNumber& other) const; 
-    bool operator<(const BigNumber& other) const;  
-    bool operator>(const BigNumber& other) const;  
-    bool operator<=(const BigNumber& other) const; 
-    bool operator>=(const BigNumber& other) const; 
+    bool operator == (const BigNumber& other) const; 
+    bool operator != (const BigNumber& other) const; 
+    bool operator < (const BigNumber& other) const;  
+    bool operator > (const BigNumber& other) const;  
+    bool operator <= (const BigNumber& other) const; 
+    bool operator >= (const BigNumber& other) const; 
 
     std::string toString() const;
 
-private:
-    std::string integer; 
-    std::string fractional; 
-    bool sign; 
 };
+
+// input and output
+std::ostream& operator<<(std::ostream& out, const BigNumber& number);
+std::istream& operator>>(std::istream& in, BigNumber& number);
 
 #endif 
