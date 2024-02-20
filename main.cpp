66a9083846b8f5ecae735cea498851dface554bc
Alenka Sharipova <alenkasharipova@MacBook-Air-Alenka.local>
2024-02-20 19:02:13 +0300

    -

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include "BigNumber.h"
+
 
 int main(void){
-    printf("Hello world!!!");
-    printf("pu pu pu");
+    BigNumber a = BigNumber("123.45");
+    BigNumber b = BigNumber("11.5");
+    std::cout << (a + b) << '\n';
     return 0;
 }
