b681e8e27fd2fa0600d5f306607f0322cf7ad554
Alenka Sharipova <alenkasharipova@MacBook-Air-Alenka.local>
2024-02-22 15:46:58 +0300

    added calc time

--- main.cpp
+++ main.cpp
@@ -2,27 +2,13 @@
 #include <string>
 #include <iostream>
 #include "BigNumber.h"
+#include <chrono>
 size_t BigNumber::precision;
 
 int main(void){
     std::cout << "Set Pi precision: ";
+    auto start = std::chrono::high_resolution_clock::now();
     std::cin >> BigNumber::precision;
-    BigNumber a = BigNumber("15");
-    BigNumber b = BigNumber("3");
-    assert(BigNumber("11111") + BigNumber("222") == BigNumber("11333"));
-    // assert(BigNumber("1") / BigNumber("2") == BigNumber("0.5"));
-    assert(BigNumber("9999") + BigNumber("1") == BigNumber("10000"));
-    assert(BigNumber("0.25") * BigNumber("0.05") == BigNumber("0.0125"));
-    assert(BigNumber("0.1") - BigNumber("0.05") == BigNumber("0.05"));
-    assert((BigNumber("100") > BigNumber("54")) == true);
-    assert(BigNumber("-100") < BigNumber("-54") == true);
-    assert(BigNumber("100") <= BigNumber("54") == false);
-    assert((BigNumber("54") >= BigNumber("54")) == true);
-    assert((BigNumber("1212121") == BigNumber("1212121")) == true);
-    assert((BigNumber("88888") != BigNumber("88888")) == false);
-
-    std::cout << (a / b) << '\n';
-
     BigNumber Pi("0");
     BigNumber step_16("1");
     for(int k = 0; k < 100; k++) {
@@ -34,5 +20,23 @@ int main(void){
         step_16 = step_16 * BigNumber("16");
     }
     std::cout << Pi << '\n';
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> all_time = end - start;
+    std::cout << "Time of working programm " << all_time.count() << '\n';
+
+    // tests
+    BigNumber a = BigNumber("15");
+    BigNumber b = BigNumber("3");
+    assert(BigNumber("11111") + BigNumber("222") == BigNumber("11333"));
+    assert(BigNumber("9999") + BigNumber("1") == BigNumber("10000"));
+    assert(BigNumber("0.25") * BigNumber("0.05") == BigNumber("0.0125"));
+    assert(BigNumber("0.1") - BigNumber("0.05") == BigNumber("0.05"));
+    assert((BigNumber("100") > BigNumber("54")) == true);
+    assert(BigNumber("-100") < BigNumber("-54") == true);
+    assert(BigNumber("100") <= BigNumber("54") == false);
+    assert((BigNumber("54") >= BigNumber("54")) == true);
+    assert((BigNumber("1212121") == BigNumber("1212121")) == true);
+    assert((BigNumber("88888") != BigNumber("88888")) == false);
+    std::cout << a << " / " << b << " = " << (a / b) << '\n';
     return 0;
 }
